35d464e5eab7710729045cf7e645b08c9f34eedc
Harsh Joshi <harshcode0907@gmail.com>
2026-02-19 23:38:03 +0530

    basic turbo prisma setup

---
+++ apps/http-backend/index.ts
@@ -0,0 +1,48 @@
+import { prismaclient } from "@repo/db/client";
+import express from "express";
+const app = express();
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+	try {
+		const users = await prismaclient.user.findMany();
+		return res.json(users);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({
+			message: `Error occurred: ${error}`,
+		});
+	}
+});
+
+app.post("/user", async (req, res) => {
+	try {
+		const { username, password } = req.body;
+		if (!username || !password) {
+			res.status(400).json({
+				message: "Username and password are required",
+			});
+		}
+
+        const user= await prismaclient.user.create({
+            data:{
+                username,
+                password
+            }
+        })
+        if(user){
+            return res.json({
+                message:"User created successfully",
+                user
+            })
+        }
+	} catch (error) {
+        return res.status(500).json({
+            message:`Error occurred:${error}`
+        })
+    }
+});
+
+app.listen(3000, () => {
+	console.log("http backend listening at 3000");
+});
